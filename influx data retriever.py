d593e16e90d9a0c0e44c3ab1e9e65d863bdd3312
sam-a-d <masamad.sust@gmail.com>
2024-12-07 21:44:43 +0100

    replaced hardcoaded credentials with env variable

--- influx data retriever.py
+++ influx data retriever.py
@@ -1,13 +1,17 @@
 from datetime import datetime
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 # You can generate an API token from the "API Tokens Tab" in the UI
-token = "EDFGsul_l3hXK9y6BMVgspGSpnvVmabw_Ly2veGjshM9B31ykmRife_KQTt2ffsqAfde8tZGMcbmxQ4_oIEEeQ=="
-org = "softArc"
-bucket = "testBuck"
-url="http://localhost:8086"
+token = os.getenv('INFLUX_ACCESS_TOKEN')
+org = os.getenv('INFLUX_ORG')
+bucket = os.getenv('INFLUX_BUCKET')
+url = os.getenv('INFLUX_URL')
 
 import pandas as pd
 with InfluxDBClient(url=url, token=token, org=org) as client:
@@ -28,8 +32,3 @@ with InfluxDBClient(url=url, token=token, org=org) as client:
     df = pd.DataFrame(data)
     print(df)
     
-    # for table in tables:
-    #     for record in table.records:
-    #         # print(dir(record))
-    #         # break
-    #         print("Time: ", record.get_time(), "field", record.get_field(), "value: ", record.get_value())
