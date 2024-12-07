839f63c4b372fb3edb391729fa05c413cd3c3eb5
sam-a-d <masamad.sust@gmail.com>
2024-12-07 23:39:52 +0100

    inplemented telegram sender v1

---
+++ telegramSender.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+import requests
+
+telegram_bot_tocken = os.getenv('TELEGRAM_BOT_API')
+
+class TelegramSender:
+
+    def __init__(self, bot_token, chat_id, message='Risky Condition'):
+        self.bot_tocken = bot_token
+        self.chat_id = chat_id
+        self.message = message
+
+        self.url = f"https://api.telegram.org/bot{self.bot_tocken}/sendMessage"
+
+    def send_message(self, **kwargs):
+
+        self.message = kwargs['message'] if 'message' in kwargs else self.message
+        self.chat_id = kwargs['chat_id'] if 'chat_id' in kwargs else self.chat_id
+
+        payload = {
+            "chat_id": self.chat_id,  # The target user's chat ID
+            "text": self.message,     # The message to send
+        }
+
+        response = requests.post(self.url, data=payload)
+
+        if response.status_code == 200:
+            return 'Sending success'
+        else:
+            return 'Sending failed'
