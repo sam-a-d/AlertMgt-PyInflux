c810f62303d925f590b1405fe6bf052804f8b2f3
sam-a-d <masamad.sust@gmail.com>
2024-12-07 21:39:15 +0100

    removed hardcoded credentials and get them from env var

--- influx data producer.py
+++ influx data producer.py
@@ -1,7 +1,10 @@
 # from random import Random
 from datetime import datetime
 import sensor
-import time
+import time, os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 
 from influxdb_client import InfluxDBClient, Point, WritePrecision
@@ -14,10 +17,10 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 # You can generate an API token from the "API Tokens Tab" in the UI
-token = "ybnvxMPciOeiyM9FgzNIKTxvcIZK8YCgfO_EC9pUHoPyclWjxhQCG_ghD3T7kwLjGWxX3kAUGkoXRAVeKEndNg=="
-org = "softArc"
-bucket = "pythonBucket"
-url = "http://localhost:8086"
+token = os.getenv('INFLUX_ACCESS_TOKEN')
+org = os.getenv('INFLUX_ORG')
+bucket = os.getenv('INFLUX_BUCKET')
+url = os.getenv('INFLUX_URL')
 
 
 with InfluxDBClient(url=url, token=token, org=org) as client:
@@ -25,14 +28,15 @@ with InfluxDBClient(url=url, token=token, org=org) as client:
     S = sensor.Sensor()
     write_api = client.write_api(write_options=SYNCHRONOUS)
 
-    for i in range(50):
+    for i in range(3):
+    
         point = Point("SensorValues")\
         .tag('risk', S.get_tag())\
         .field("Temp", S.getTemp())\
         .field("Hum", S.getHumidity())\
         .field("SoilMoist", S.getSoilMoist())\
 
-        time.sleep(.2)
+        time.sleep(3)
         write_api.write(bucket, org, point)
 
 client.close()
