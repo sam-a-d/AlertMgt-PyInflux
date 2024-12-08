9e4caa2cba9e38ed9360a1c942a5d62c6d79ab29
sam-a-d <masamad.sust@gmail.com>
2024-12-08 18:40:38 +0100

    replace influx data producer.py with dataProducer.py

--- dataProducer.py
+++ dataProducer.py
@@ -7,16 +7,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+# You can generate an API token from the "API Tokens Tab" in the U
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-# You can generate an API token from the "API Tokens Tab" in the UI
-from datetime import datetime
-
-from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
-
-# You can generate an API token from the "API Tokens Tab" in the UI
 token = os.getenv('INFLUX_ACCESS_TOKEN')
 org = os.getenv('INFLUX_ORG')
 bucket = os.getenv('INFLUX_BUCKET')
@@ -40,6 +34,7 @@ with InfluxDBClient(url=url, token=token, org=org) as client:
         .field("wind",S.getWindSpeed())\
         
         time.sleep(1)
+        print('Okay till')
         write_api.write(bucket, org, point)
 
 client.close()
